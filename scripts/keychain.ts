4bb35568fc6ff250bf9c641e11be71ce933b5959
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2026-01-21 17:04:26 +0000

    jan2026 - printing value of v_Data

--- scripts/keychain.ts
+++ scripts/keychain.ts
@@ -22,10 +22,40 @@ function hook(name: string): void {
         onEnter(args) {
             try {
                 const dict = new ObjC.Object(args[0]);
-                console.log(`\n[${name}]`);
-                console.log(dict.toString());
+
+                // Ensure it's actually an NSDictionary
+                if (!dict.allKeys) {
+                    console.log("[!] Not an NSDictionary");
+                    return;
+                }
+
+                const keys = dict.allKeys();
+                const count = keys.count();
+
+                console.log(`${name}\n{`);
+                for (let index = 0; index < count; index++) {
+                    const k = keys.objectAtIndex_(index);
+                    const v = dict.objectForKey_(k);
+
+                    const keyStr = k ? k.toString() : "<null>";
+                    const valStr = v ? v.toString() : "<null>";
+
+                    // Decode password data
+                    if (keyStr === "v_Data") {
+                        const nsString = ObjC.classes.NSString.alloc();
+                        const decodedValue = nsString
+                            .initWithData_encoding_(v, 4) // NSUTF8StringEncoding
+                            .toString();
+                        console.log(`\t${keyStr} : ${decodedValue}`);
+
+                    }
+                    else {
+                        console.log(`\t${keyStr} : ${valStr}`);
+                    }
+                }
+                console.log(`}`);
             } catch (e) {
-                console.log(`[${name}] unable to parse dictionary:`, e);
+                console.log("unable to parse dictionary:", e);
             }
         }
     });
