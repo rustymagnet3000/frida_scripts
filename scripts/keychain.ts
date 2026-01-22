b251f52d060f4d2040e97734908a7abb4e9290de
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2026-01-22 10:10:11 +0000

    * jan2026 - delete general passwords from keychain

--- scripts/keychain.ts
+++ scripts/keychain.ts
@@ -1,3 +1,7 @@
+// gena → encrypted item key (never decryptable)
+// agrp → access group
+// pdmn → protection class
+// v_Data is the secret
 import ObjC from "frida-objc-bridge";
 
 if (!ObjC.available) {
@@ -22,10 +26,40 @@ function hook(name: string): void {
         onEnter(args) {
             try {
                 const dict = new ObjC.Object(args[0]);
-                console.log(`\n[${name}]`);
-                console.log(dict.toString());
+
+                // Ensure it's actually an NSDictionary
+                if (!dict.allKeys) {
+                    console.log("[!] Not an NSDictionary");
+                    return;
+                }
+
+                const keys = dict.allKeys();
+                const count = keys.count();
+
+                console.log(`${name}\n{`);
+                for (let index = 0; index < count; index++) {
+                    const k = keys.objectAtIndex_(index);
+                    const v = dict.objectForKey_(k);
+
+                    const keyStr = k ? k.toString() : "<null>";
+                    const valStr = v ? v.toString() : "<null>";
+
+                    // Decode password data
+                    if (keyStr === "v_Data") {
+                        const nsString = ObjC.classes.NSString.alloc();
+                        const decodedValue = nsString
+                            .initWithData_encoding_(v, 4) // NSUTF8StringEncoding
+                            .toString();
+                        console.log(`\t${keyStr} : ${decodedValue}`);
+
+                    }
+                    else {
+                        console.log(`\t${keyStr} : ${valStr}`);
+                    }
+                }
+                console.log(`}`);
             } catch (e) {
-                console.log(`[${name}] unable to parse dictionary:`, e);
+                console.log("unable to parse dictionary:", e);
             }
         }
     });
